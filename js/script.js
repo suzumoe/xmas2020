c7ab0806f1d5a978a7dadc0129f1ae68d44800e1
suzukimoena <mona.trombone@gmail.com>
2021-07-29 18:30:17 +0900

    add style

---
+++ js/script.js
@@ -0,0 +1,31 @@
+
+$(function(){
+
+  $(".main-visual").snowfall(
+    {
+        // 雪の量 (数値)
+        flakeCount : 120,
+        // 色 (RGB)
+        // flakeColor : "",
+        // z-indexの値
+        flakeIndex : "888",
+        // 最小サイズ （数値）
+        minSize : 2,
+        // 最大サイズ（数値）
+        maxSize : 9,
+        // 最低速度（数値）
+        minSpeed : 1,
+        // 最高速度（数値）
+        maxSpeed : 3,
+        // 雪の形を丸にする（boolean）
+        round : true,
+        // 影をつける（boolean）
+        shadow : false,
+        // 要素に積もらせる場合
+        collection : "積もらせる要素",
+        // オリジナル画像を使用する場合
+        // image : "画像へのパス"
+    }
+  );
+
+});
